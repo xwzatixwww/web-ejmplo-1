e9a0ea96da5f897226b2f517fd05c5771f776751
xwzatixwww <xwzadquiel@gmail.com>
2026-01-23 10:20:45 -0600

    Add files via upload

---
+++ TIC6/WEB/JAVASCRIPT/script.js
@@ -0,0 +1,7 @@
+//obtener el boto por su ID que se puso 
+const boton1=document.getElementById('milkiwey');
+
+//Agregar un evento al boton
+boton1.addEventListener('click',function(){
+    alert("AHUEVOOO");
+});
